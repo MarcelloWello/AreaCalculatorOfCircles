e121a33d15002a79f5c0db02b6eb38f90a6371c1
Marcello Ferguson <marcello.ferguson@live.com>
2019-11-15 13:08:30 +1300

    Add project files.

---
+++ AreaCalculatorOfCircles/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace AreaCalculatorOfCircles
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double radius = 2.5;
+            if (args.Length == 1)
+            {
+                radius = double.Parse(args[0]);
+            }
+            double radiusSquared = Math.Pow(radius, 2.0);
+            double area = radiusSquared * Math.PI;
+            Console.WriteLine($"I was given a circle with a radius of {radius} and the area equals {area} ");
+
+        }
+    }
+}
